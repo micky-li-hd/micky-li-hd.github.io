ba218ee12cc33e183cb594f6c21c992241fb652f
micky-li-hd <3196859347@qq.com>
2026-01-09 19:15:25 +0800

    gallery: smoother wrap with preload [1,2] and mfp-fade

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -76,7 +76,8 @@ $(document).ready(function(){
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      // Preload 1 previous and 2 next images for smoother wrap-around
+      preload: [1,2]
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
@@ -84,7 +85,8 @@ $(document).ready(function(){
     removalDelay: 500, // Delay in milliseconds before popup is removed
     // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-zoom-in',
+    // Use a subtle fade for smoother transitions
+    mainClass: 'mfp-fade',
     callbacks: {
       beforeOpen: function() {
         // just a hack that adds mfp-anim class to markup
